3df0ae16b6d572f94b05132a61a84b5eb0640aee
rodrigo <javierluis15975@hotmail.com>
2025-02-13 22:47:48 -0500

    update 1 file and create 8 files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import mikroOrmConfig from './mikro-orm.config'
 import { AuthorRepository } from './repository'
 import { Author } from './entity'
 import { CrawlerPlaywrightModule } from './crawler-playwright/crawler-playwright.module'
+import { ProductsModule } from './products/products.module'
 @Module({
   imports: [
     MikroOrmModule.forRoot(mikroOrmConfig),
     MikroOrmModule.forFeature([Author]),
     CrawlerPlaywrightModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService, AuthorRepository],
